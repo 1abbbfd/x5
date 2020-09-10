23300e5ad1c88a200667c4b42ae7aea107d7d3ab
Vladislav Raspopin <Vladislav.Raspopin@x5.ru>
2020-09-10 18:14:32 +0300

    fix

--- src/java/oop/lesson3/animal/Main.java
+++ src/java/oop/lesson3/animal/Main.java
@@ -3,7 +3,7 @@ package oop.lesson3.animal;
 public class Main {
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
+        Animal cat = new Cat();
         Animal dog = new Dog();
         Animal horse = new Horse();
         Animal[] animals = new Animal[]{cat, dog, horse};
